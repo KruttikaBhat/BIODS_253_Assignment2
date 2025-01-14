fc427d5da9a0795f6a437255d8d25f1932af4361
Tina Mai <tinabmai@gmail.com>
2025-01-14 14:43:06 -0800

    main rectangle for house

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import turtle
+
+# Draw main rectangle for house frame
+turtle.penup()
+turtle.goto(-200, -200)  # Starting position for house frame
+turtle.pendown()
+
+# Draw rectangle
+for _ in range(2):
+    turtle.forward(400)  # Width of house
+    turtle.left(90)
+    turtle.forward(200)  # Height of house
+    turtle.left(90)
+
+turtle.done()

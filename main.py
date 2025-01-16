34d91c0cad244d11e359fbc46054c3b926ec2390
Kruttika Bhat <gkb15997@gmail.com>
2025-01-15 17:21:38 -0800

    added a triangle function for tree tops

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import turtle
 
+def draw_triangle(tip, side_length, color):
+    """
+    Draws an equilateral triangle using the turtle module.
+
+    Parameters:
+    side_length (int or float): The length of each side of the triangle.
+    """
+    tip.fillcolor(color)
+    tip.begin_fill() 
+    for _ in range(3):
+        tip.forward(side_length)  # Move forward by the specified side length
+        tip.left(120)  # Turn the turtle 120 degrees to the left
+
+    tip.end_fill()
+
+
 # Draw main rectangle for house frame
 turtle.penup()
 turtle.goto(-200, -200)  # Starting position for house frame
@@ -11,5 +27,10 @@ for _ in range(2):
     turtle.left(90)
     turtle.forward(200)  # Height of house
     turtle.left(90)
+
+turtle.penup()
+
+turtle.goto(-400, -100)
+draw_triangle(turtle, 100, "green")
 
 turtle.done()

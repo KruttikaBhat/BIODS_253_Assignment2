8319e31db42c21cc5cd98882dbf00ab074b589b2
robinncai <ruoqing.cai@gmail.com>
2025-01-20 17:56:06 -0800

    add window functions

--- main.py
+++ main.py
@@ -12,4 +12,26 @@ for _ in range(2):
     turtle.forward(200)  # Height of house
     turtle.left(90)
 
+
+# function to draw a rectangle with specified starting position
+def draw_rectangle(start,width, height):
+
+    # Lift the pen to avoid drawing
+    turtle.penup()
+
+    turtle.goto(start)
+    turtle.pendown()
+    for _ in range(2):
+        turtle.forward(width)
+        turtle.left(90)
+        turtle.forward(height)
+        turtle.left(90)
+    
+
+
+
+#draw 2 windows
+draw_rectangle((-150, -100), 50, 50)
+draw_rectangle((100, -100), 50, 50)
+
 turtle.done()
